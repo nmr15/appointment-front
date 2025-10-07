1417288ca76306a0aaa61d099601370c5c7009bd
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-07 11:38:19 -0700

    update Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import Hero from '../Components/Hero'
+import ServicesSmall from '../Components/ServicesSmall'
+import Welcome from '../Components/Welcome'
 
 const Home = () => 
 {
   return (
     <>
       <Hero />
+      <ServicesSmall />
+      <Welcome />
     </>
   )
 }
