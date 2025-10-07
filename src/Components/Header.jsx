def6c33d40dc5912d8ecf4bca421226888580e8a
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-07 10:39:40 -0700

    update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,16 @@ const Header = () =>
     <>
       <header className="header">
         <div className="header__contact">
-          
+          <div className="row">
+            <div>
+              <img src="https://placehold.co/16x16" alt="phone number" />
+              <p>(800)-123-4567</p>
+            </div>
+            <div>
+              <img src="https://placehold.co/16x16" alt="address" />
+              <p>1111 Fake St, Los Angeles, CA</p>
+            </div>
+          </div>
         </div>
       </header>
     </>
