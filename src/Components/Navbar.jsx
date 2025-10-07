eca4be9bbf8cc228e957fd7144eb294aa257e681
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-07 11:27:46 -0700

    filled in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,6 +8,13 @@ const Navbar = () =>
         <div className="navbar__logo">
           <img src="https://placehold.co/180x60" alt="" />
         </div>
+        <div className="navbar__list">
+          <a href="#" className="navbar__link">Home</a>
+          <a href="#" className="navbar__link">Services</a>
+          <a href="#" className="navbar__link">Doctors</a>
+          <a href="#" className="navbar__link">Appointments</a>
+          <a href="#" className="navbar__link">Sign in/Sign up</a>
+        </div>
       </nav>
     </>
   )
