24dddbe934f573aca28f5a0ff21e905a3f0daa12
Niel <niel_ravelo@yahoo.com>
2025-10-11 11:59:22 -0700

    some updates

--- src/Components/ServicesSmall.jsx
+++ src/Components/ServicesSmall.jsx
@@ -1,11 +1,39 @@
 import './ServicesSmall.scss'
+import { FaUserNurse, FaSyringe } from "react-icons/fa";
+import { FaUserDoctor } from "react-icons/fa6";
 
 const ServicesSmall = () => 
 {
   return (
     <>
-      <div className="servicesSm">
-        <h3>Services Small</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <div className="serviceSm">
+              <span className="serviceSm__icon">
+                <FaUserNurse />
+              </span>
+              <h4 className="serviceSm__title">Lab Services</h4>
+              <p className="serviceSm__desc">Fast, accurate diagnostic testing to support timely and effective medical care.</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="serviceSm">
+              <span className="serviceSm__icon">
+                <FaUserDoctor />
+              </span>
+              <h4 className="serviceSm__title">Medical Professionals</h4>
+              <p className="serviceSm__desc">Our experienced medical professionals provide compassionate, expert care to support your health and well-being.</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <span className="serviceSm__icon">
+              <FaSyringe />
+            </span>
+            <h4 className="serviceSm__title">Immunizations</h4>
+            <p className="serviceSm__desc">Trusted immunization services to keep you and your family protected, healthy, and up to date.</p>
+          </div>
+        </div>
       </div>
     </>
   )
