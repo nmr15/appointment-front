74ade1fd0b8775357bf08d0817cb9084ddefa7d8
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-15 10:43:39 -0700

    update ServiceSmall

--- src/Components/ServicesSmall.jsx
+++ src/Components/ServicesSmall.jsx
@@ -2,10 +2,17 @@ import './ServicesSmall.scss'
 import { FaUserNurse, FaSyringe } from "react-icons/fa";
 import { FaUserDoctor } from "react-icons/fa6";
 
-const ServicesSmall = () => 
+const ServicesSmall = ({ icon, title, desc }) => 
 {
   return (
     <>
+      <div className="serviceSm">
+        <span className="serviceSm__icon">
+          {icon}
+        </span>
+        <h4 className="serviceSm__title">{title}</h4>
+        <p className="serviceSm__desc">{desc}</p>
+      </div>
       <div className="container">
         <div className="row">
           <div className="col-4">
---
+++ src/Components/ServicesSmallList.jsx
@@ -0,0 +1,14 @@
+const ServicesSmallList = () =>
+{
+  return(
+    <>
+      <div className="container">
+        <div className="row">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ServicesSmallList
