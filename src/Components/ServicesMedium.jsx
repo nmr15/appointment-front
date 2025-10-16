d8701d1e38d09dd259ed73cc28de7c38ea8577fc
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-16 09:07:07 -0700

    update servicesMd

--- src/Components/ServicesMedium.jsx
+++ src/Components/ServicesMedium.jsx
@@ -1,16 +1,35 @@
-import './ServicesMedium.scss'
+import "./ServicesMedium.scss";
+import { FaBriefcaseMedical } from "react-icons/fa";
 
-const ServicesMedium = () => 
-{
+const ServicesMedium = () => {
   return (
     <>
       <section>
-        <div className="servicesMd">
-          <h3>Services Medium</h3>
+        <div className="container">
+          <div className="col-4">
+            <div className="serviceMd">
+              <span className="serviceMd__icon">
+                <FaBriefcaseMedical />
+              </span>
+              <h4>Lorem ipsum</h4>
+              <p>
+                Curabitur scelerisque metus tortor, at condimentum neque gravida
+                et.
+              </p>
+              <ul className="serviceMd__list">
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+                <li>Item 4</li>
+                <li>Item 5</li>
+                <li>Item 6</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default ServicesMedium
+export default ServicesMedium;
