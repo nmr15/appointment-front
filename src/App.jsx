3e2497d1f87d8341e01fdaf966af490c84f3ede1
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-20 10:02:36 -0700

    imported Routes and Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Routes, Route } from 'react-router-dom'
 import './App.scss'
 import axios from 'axios';
 import Home from './Pages/Home'
