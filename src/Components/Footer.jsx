7d8837c84b3e22d2f1b8acc21f508cc16549a542
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-20 09:33:23 -0700

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,12 @@
 import './Footer.scss'
 import { FaLocationDot, FaPhone, FaEnvelope } from "react-icons/fa6";
 
+const blog = [
+  {id: "1", img: "https://placehold.co/50x50", link: "Lorem ipsum", date: "Lorem ipsum"},
+  {id: "2", img: "https://placehold.co/50x50", link: "Lorem ipsum", date: "Lorem ipsum"},
+  {id: "3", img: "https://placehold.co/50x50", link: "Lorem ipsum", date: "Lorem ipsum"}
+]
+
 const contact = [
   {id: "1", icon: <FaLocationDot />, info: "1111 Fake St, Los Angeles, CA"},
   {id: "2", icon: <FaPhone />, info: "(800)-123-4567"},
@@ -22,6 +28,15 @@ const Footer = () =>
             <div className="col-4">
               <div className="footer__blog">
                 <h3>Blog Post</h3>
+                <ul className="footer__blog-list">
+                  {blog.map((blog) => (
+                    <li className="footer__blog-item" key={blog.id}>
+                      <img src={blog.img} alt="" />
+                      <a href="#">{blog.link}</a>
+                      <p>{blog.date}</p>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
             <div className="col-4">
