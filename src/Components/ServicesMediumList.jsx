18a5490ff78d346a7596c0a9a70f3b7c63e29312
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-20 08:41:09 -0700

    added class to icons

--- src/Components/ServicesMediumList.jsx
+++ src/Components/ServicesMediumList.jsx
@@ -4,8 +4,8 @@ import { GiBarefoot, GiMedicinePills } from "react-icons/gi";
 
 const services = [
   {id: "1", icon: <FaXRay className="icon" />, title: "Radiology", desc: "Lorem ipsum", list: ["X-ray", "CT scan", "PET scan", "MRI", "Ultrasound", "Mammogram"]},
-  {id: "2", icon: <GiBarefoot />, title: "Rehabilitation", desc: "Lorem ipsum", list: ["Physical Therapy", "Occupation Therapy", "Speech Therapy", "Aquatic Therapy", "Wound Care", "Pediatric Rehabilitation"]},
-  {id: "3", icon: <GiMedicinePills />, title: "Cancer Treatment", desc: "Lorem ipsum", list: ["Chemotherapy", "Radiation Therapy", "Cancer Surgery", "Hormone Therapy", "Immunotherapy", "Targeted Therapy"]}
+  {id: "2", icon: <GiBarefoot className="icon" />, title: "Rehabilitation", desc: "Lorem ipsum", list: ["Physical Therapy", "Occupation Therapy", "Speech Therapy", "Aquatic Therapy", "Wound Care", "Pediatric Rehabilitation"]},
+  {id: "3", icon: <GiMedicinePills className="icon" />, title: "Cancer Treatment", desc: "Lorem ipsum", list: ["Chemotherapy", "Radiation Therapy", "Cancer Surgery", "Hormone Therapy", "Immunotherapy", "Targeted Therapy"]}
 ]
 
 const ServicesMediumList = () => 
