7b8712602848588bb93094f1085ee64256a50771
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-20 08:56:49 -0700

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,9 +4,27 @@ const Footer = () =>
 {
   return (
     <>
-      <footer className="footer">
-        <h3>Footer</h3>
-      </footer>
+      <div className="container">
+        <footer className="footer">
+          <div className="row">
+            <div className="col-4">
+              <div className="footer__left">
+                <h3>Footer Left</h3>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="footer__middle">
+                <h3>Footer Middle</h3>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="footer__right">
+                <h3>Footer Right</h3>
+            </div>
+            </div>
+          </div>
+        </footer>
+      </div>
     </>
   )
 }
