282ef8cad1096b9745ac2e920794884f0e5bb13b
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-21 09:36:53 -0700

    update Doccard

--- src/Components/DoctorCard.jsx
+++ src/Components/DoctorCard.jsx
@@ -6,7 +6,7 @@ const DoctorCard = () =>
     <>
       <div className="docCard">
         <div className="docCard__img">
-          <img src="https://placehold.co/230x230?text=Doctor" alt="doctor_image" />
+          <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
         </div>
         <div className="docCard__content">
           <h3>Dr. Alex Carter</h3>
