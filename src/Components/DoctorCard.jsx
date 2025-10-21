6252c911a58324e1aa6db9734debe49e2be4a0c6
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-21 10:01:20 -0700

    update

--- src/Components/DoctorCard.jsx
+++ src/Components/DoctorCard.jsx
@@ -4,17 +4,46 @@ const DoctorCard = () =>
 {
   return(
     <>
-      <div className="col-3">
-        <div className="docCard">
-            <div className="docCard__img">
-                <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
-            </div>
-            <div className="docCard__content">
-                <h3>Dr. Alex Carter</h3>
-                <span>Internal Medicine</span>
+        <div className="row">
+            <div className="col-3">
+                <div className="docCard">
+                    <div className="docCard__img">
+                        <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
+                    </div>
+                    <div className="docCard__content">
+                        <h3>Dr. Alex Carter</h3>
+                        <span>Internal Medicine</span>
+                    </div>
+                </div>
+                <div className="docCard">
+                    <div className="docCard__img">
+                        <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
+                    </div>
+                    <div className="docCard__content">
+                        <h3>Dr. Alex Carter</h3>
+                        <span>Internal Medicine</span>
+                    </div>
+                </div>
+                <div className="docCard">
+                    <div className="docCard__img">
+                        <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
+                    </div>
+                    <div className="docCard__content">
+                        <h3>Dr. Alex Carter</h3>
+                        <span>Internal Medicine</span>
+                    </div>
+                </div>
+                <div className="docCard">
+                    <div className="docCard__img">
+                        <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
+                    </div>
+                    <div className="docCard__content">
+                        <h3>Dr. Alex Carter</h3>
+                        <span>Internal Medicine</span>
+                    </div>
+                </div>
             </div>
         </div>
-      </div>
     </>
   )
 }
