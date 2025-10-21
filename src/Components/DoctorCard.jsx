026581c58a6b2c3539ae802f50b8118fd6133dd0
Nathaniel Ravelo <48196807+nmr15@users.noreply.github.com>
2025-10-21 09:41:10 -0700

    update

--- src/Components/DoctorCard.jsx
+++ src/Components/DoctorCard.jsx
@@ -4,13 +4,15 @@ const DoctorCard = () =>
 {
   return(
     <>
-      <div className="docCard">
-        <div className="docCard__img">
-          <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
-        </div>
-        <div className="docCard__content">
-          <h3>Dr. Alex Carter</h3>
-          <span>Internal Medicine</span>
+      <div className="col-3">
+        <div className="docCard">
+            <div className="docCard__img">
+            <img src="https://placehold.co/200x200?text=Doctor" alt="doctor_image" />
+            </div>
+            <div className="docCard__content">
+            <h3>Dr. Alex Carter</h3>
+            <span>Internal Medicine</span>
+            </div>
         </div>
       </div>
     </>
